e89e1becaf4f0ec90a378ac0ee4c83174ac3023d
Basord <basord13@gmail.com.com>
2015-10-28 21:04:08 +0100

    Remplissage du prototype de box pour les émissions et probleme du commit précédent corrigé

--- accueil.php
+++ accueil.php
@@ -1,5 +1,15 @@
 <section id="accueil">
+	<div id="container-titres">
+		<div id="container-titre-programme">
+			<div id="titre-programme"></div>
+		</div>
+		<div id="container-titre-news">
+			<div id="titre-news"></div>
+		</div>
+	</div>
+
 	<div id="places">
+		
 		<div id="box-place">
 			<?php 
 			get_template_part('boxes');
--- boxes.php
+++ boxes.php
@@ -1,15 +1,55 @@
 <div class="box">
 	<div class="left-part">
 		<div class="logo-streamer">
-			<a href="#"><img src=""></a>
+			<a href="#"><img src="" alt="logo"></a>
 		</div>
 		<div class="calendrier-stream">
 
-			<a href="#"></a>
-			<p>CALENDRIER</p>
+			<a href="#">CALENDRIER</a>
+			
 		</div>
 	</div>
-	
+	<div class="right-part">
+		<div class="groupe-emissions">
+			<div class="fleche-carrousssel-left"><img src="/workspace/Stream-reference/wordpress/wp-content/themes/Stream-reference/images/fleche-carroussel-left.png" id="imgfleche-carroussel-left"></div> <! apparemment c'est avec une div qu'on peut faire un carrousel qui permet de naviguer parmis les émissions sur une memepage (voir "programme-relevision .org) !>
+			<div class="emissions">
+				<div class="preview-and-title">
+					<div class="imgstream">image preview du stream</div>
+					<div class="titrestream">titre du stream</div>
+				</div>
+				<div class="infos-complementaires">
+					<div class="reseaux-sociaux">
+						<a href="#"><img src="">Fb</a>
+						<a href="#"><img src="">t</a>
+						<a href="#"><img src="">Yt</a>
+					</div>
+					<div class="dureestream">Durée et heure de début/Fin</div>
+					<div class="nomstreamers">Noms streamers</div>
+					<div class="nbviewers">Nb de viewers</div>
+					<div class="lienstream"><a href="#">Stream</a></div>
+				</div>
+			</div>
+			<div class="emissions">
+				<div class="preview-and-title">
+					<div class="imgstream">image preview du stream</div>
+					<div class="titrestream">titre du stream</div>
+				</div>
+				<div class="infos-complementaires">
+					<div class="reseaux-sociaux">
+						<a href="#"><img src="">Fb</a>
+						<a href="#"><img src="">t</a>
+						<a href="#"><img src="">Yt</a>
+					</div>
+					<div class="dureestream">Durée et heure de début/Fin</div>
+					<div class="nomstreamers">Noms streamers</div>
+					<div class="nbviewers">Nb de viewers</div>
+					<div class="lienstream"><a href="#">Stream</a></div>
+				</div>
+				
+			</div>
+			<div class="fleche-carrousssel-right"><img src="/workspace/Stream-reference/wordpress/wp-content/themes/Stream-reference/images/fleche-carroussel-right.png" id="imgfleche-carroussel-right"></div>
+		</div>
+	</div>
 </div>
 
 
