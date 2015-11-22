606f8fbfaec810b48184aab6d807445c91de4299
Basord <basord13@gmail.com.com>
2015-11-22 11:58:55 +0100

    Ajout de commentaires et renommage de varaiables (juste pour que ca soit plus propre)

--- nbviewers.php
+++ nbviewers.php
@@ -1,10 +1,10 @@
 <?php
 
-$channels3 = array("MilleniumTVLoL,LEKADAYZ,Mactorlol,Furiie,Gardoum,Rekkles,yuniko0720,OgamingLoL,jiahao2") ;                         
-$callAPI3 = implode(",",$channels3);                                                                
-$dataArray3 = json_decode(@file_get_contents('https://api.twitch.tv/kraken/streams?channel=' . $callAPI3), true);
+$channels = array("MilleniumTVLoL,LEKADAYZ,Mactorlol,Furiie,Gardoum,Rekkles,yuniko0720,OgamingLoL,jiahao2") ;                         
+$callAPI = implode(",",$channels);                                                                
+$dataArray = json_decode(@file_get_contents('https://api.twitch.tv/kraken/streams?channel=' . $callAPI), true);
 
-foreach($dataArray3['streams'] as $mydata){                      
+foreach($dataArray['streams'] as $mydata){                             //Boucle "temoin" pour verifier que les données recoltées dans les autres boucles sont correctes (a supprimer une fois que tout est terminé)
     if($mydata['_id'] != null){
         $name      = $mydata['channel']['display_name'];                  
         $viewers =   $mydata['viewers'];                                  
@@ -24,10 +24,10 @@ foreach($dataArray3['streams'] as $mydata){
 
 
 $i = 0;                                                                                            //Boucle pour trouver le nombre de viewers d'un stream enparticulier
-for ($dataArray3['streams'][$i]; $i < 100; $i++) {                                                 //Changer le <100 si il y a plus de 100 streamers     
-	if($dataArray3['streams'][$i] != null ){
-		if (in_array($created_at = '2015-11-22T09:50:34Z', $dataArray3['streams'][$i])) {          
-			echo $dataArray3['streams'][$i]['viewers'];
+for ($dataArray['streams'][$i]; $i < 100; $i++) {                                                 //Changer le <100 si il y a plus de 100 streamers     
+	if($dataArray['streams'][$i] != null ){
+		if (in_array($created_at = '2015-11-22T09:50:34Z', $dataArray['streams'][$i])) {          
+			echo $dataArray['streams'][$i]['viewers'];                                           //Enlever le "echo" et mettre a la place un nom de varaible différent pour chaque stremer qui sera la variable qui recuperera le nombre de viewers du streamer
 		}
 	}
 }
@@ -36,10 +36,10 @@ $i = 0;
 echo "<br/>";
 
 
-for ($dataArray3['streams'][$i]; $i < 100; $i++) { 
-	if($dataArray3['streams'][$i] != null ){
-		if (in_array($created_at = '2015-11-22T09:58:04Z', $dataArray3['streams'][$i])) {          
-			echo $dataArray3['streams'][$i]['viewers'];
+for ($dataArray['streams'][$i]; $i < 100; $i++) { 
+	if($dataArray['streams'][$i] != null ){
+		if (in_array($created_at = '2015-11-22T09:58:04Z', $dataArray['streams'][$i])) {          
+			echo $dataArray['streams'][$i]['viewers'];
 		}
 	}
 }
